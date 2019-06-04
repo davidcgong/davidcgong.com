c96da7b9d892dae90612f64d12b2157f2e437643
David Gong <dgong@bp-dgong-01.am.sony.com>
2019-06-04 15:39:38 -0700

    Fixed up google analytics

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -10,11 +10,30 @@ type Props = {
   description?: string
 };
 
+const injectGA = () => {
+  if (typeof window == 'undefined') {
+    return;
+  }
+  window.dataLayer = window.dataLayer || [];
+  function gtag() {
+    window.dataLayer.push(arguments);
+  }
+  gtag('js', new Date());
+
+  gtag('config', 'UA-124026237-2');
+};
+
 const Layout = ({ children, title, description }: Props) => (
   <div className={styles.layout}>
     <Helmet>
       <html lang="en" />
       <title>{title}</title>
+      {/* Global site tag (gtag.js) - Google Analytics */}
+      <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=UA-124026237-2"
+      />
+      <script>{injectGA()}</script>
       <meta name="description" content={description} />
     </Helmet>
     {children}
