34a4a5046e97ad46e887d251044d042bb620ae2f
davidcgong <davidcgong@gmail.com>
2020-06-06 13:08:53 -0400

    add automatic cache refresh

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,3 +3,13 @@
 require('./src/assets/scss/init.scss');
 
 exports.onClientEntry = () => {};
+// refresh cache on visit
+exports.onServiceWorkerUpdateFound = () => {
+    if (
+      window.confirm(
+        "This site has been updated with new data. Do you wish to reload the site to get the new data?"
+      )
+    ) {
+      window.location.reload(true);
+    }
+  };
